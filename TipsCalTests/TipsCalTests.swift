2beda9ada706792873f963cf726c76259de509c1
Apple <TienLe@les-macbook-pro.local>
2016-06-19 17:42:32 +0700

    Initial Commit

---
+++ TipsCalTests/TipsCalTests.swift
@@ -0,0 +1,36 @@
+//
+//  TipsCalTests.swift
+//  TipsCalTests
+//
+//  Created by Le Huynh Anh Tien on 6/19/16.
+//  Copyright © 2016 Tien Le. All rights reserved.
+//
+
+import XCTest
+@testable import TipsCal
+
+class TipsCalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
